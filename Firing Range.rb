e97f478258d4714c2c1491397790e7c680ef91ee
Brandon Presley <presley.brandon@gmail.com>
2013-01-04 18:58:32 -0700

    Discarding this commit. All kinda broken, but some good ideas.

--- Firing Range.rb
+++ Firing Range.rb
@@ -2,11 +2,26 @@ Shoes.app :title => "Firing Range", :width => 300, :height => 300 do #these are 
 	stack do # vertical stack
 		flow do #horizontal flow of elements
 			caption "Start at: "
-			edit_box do |e| # http://shoesrb.com/manual/EditBox.html
-				@counter.text = e.text.size
-			end
+			@start_counter = edit_line
+			#edit_box :height => 30, :width => 50 do |e| # http://shoesrb.com/manual/EditBox.html
+			#	@start_counter.text = e.text.size
+			#end
+			#@counter = text("0")
+			#para @counter
 		end # back in the stack
-		@counter = strong("0")
-			para @counter, " characters" 
+		flow do
+		caption "End at:  "
+		 
+			edit_box :height => 30, do |e|
+				@end_counter = e.text
+			end
+		end
+		#@counter = strong("0")
+			para @start_counter.size, ", and ending at ", @end_counter
+		button "Submit" do
+			alert @start_counter.text
+
+		end
+
 	end
 end
