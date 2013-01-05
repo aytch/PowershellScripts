6ad58a19ab314f95564aa82e02b01b76acdc57cd
Brandon Presley <presley.brandon@gmail.com>
2013-01-04 18:58:44 -0700

    Revert "Discarding this commit. All kinda broken, but some good ideas."
    This reverts commit e97f478258d4714c2c1491397790e7c680ef91ee.

--- Firing Range.rb
+++ Firing Range.rb
@@ -2,26 +2,11 @@ Shoes.app :title => "Firing Range", :width => 300, :height => 300 do #these are 
 	stack do # vertical stack
 		flow do #horizontal flow of elements
 			caption "Start at: "
-			@start_counter = edit_line
-			#edit_box :height => 30, :width => 50 do |e| # http://shoesrb.com/manual/EditBox.html
-			#	@start_counter.text = e.text.size
-			#end
-			#@counter = text("0")
-			#para @counter
-		end # back in the stack
-		flow do
-		caption "End at:  "
-		 
-			edit_box :height => 30, do |e|
-				@end_counter = e.text
+			edit_box do |e| # http://shoesrb.com/manual/EditBox.html
+				@counter.text = e.text.size
 			end
-		end
-		#@counter = strong("0")
-			para @start_counter.size, ", and ending at ", @end_counter
-		button "Submit" do
-			alert @start_counter.text
-
-		end
-
+		end # back in the stack
+		@counter = strong("0")
+			para @counter, " characters" 
 	end
 end
