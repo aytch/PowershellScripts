60443b318354c40dbe3782965f8904ee068c614e
Brandon Presley <aytch>
2013-01-09 07:15:03 -0700

    Created function for argument. Untested.

--- Firing Range.rb
+++ Firing Range.rb
@@ -1,10 +1,17 @@
+def ps_args {
+	@arg1 = @counter.text
+	@arg2 = @counter2.text
+	@results = system('powershell C:\PowerShellScripts\Close-Range.ps1 #{@arg1} #{@arg2}')
+	alert @results
+}
+
 Shoes.app(:title => "Firing Range", :width => 300, :height => 300 ) do #these are criteria for the window
 	stack do # vertical stack
 
 		flow do #horizontal flow of elements
 			caption "Start at: "
 			edit_box :height => 30 do |e| # http://shoesrb.com/manual/EditBox.html
-				@counter.text = e.text.size
+				@counter.text = e.text()
 			end
 		end # back in the stack
 
@@ -19,7 +26,8 @@ Shoes.app(:title => "Firing Range", :width => 300, :height => 300 ) do #these ar
 		para @counter, " what? ", @counter2, "."
 
 		button "Submit" do
-			alert '@counter is the size, and @counter2 is the actual contents. *\o/*'
+			ps_args()
 		end
 	end
 end
+
