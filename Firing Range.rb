095b3f1ed50704582b4d772626d22acb5096ce09
aytch <iamnotadj@gmail.com>
2013-01-28 14:20:25 -0700

    Update Firing Range.rb

--- Firing Range.rb
+++ Firing Range.rb
@@ -1,25 +1,35 @@
-Shoes.app(:title => "Firing Range", :width => 300, :height => 300 ) do #these are criteria for the window
-	stack do # vertical stack
+Shoes.app(:title => "Firing Range", :width => 300, :height => 300 ) do 
+	background lightslategray
+	fill lightblue
+	rect(:center => false, :top => 5, :left => 5, :height => 280, :width => 280, :curve => 12)
+
+	stack :margin => 10 do # vertical stack
 
 		flow do #horizontal flow of elements
 			caption "Start at: "
-			edit_box :height => 30 do |e| # http://shoesrb.com/manual/EditBox.html
-				@counter.text = e.text.size
+			edit_box :height => 30, :width => 150 do |e| 
+				@counter.text = e.text()
+				start = @counter.text.to_s
 			end
 		end # back in the stack
 
 		flow do 
 			caption "End at:  "
-			edit_box :height => 30 do |e|
+			edit_box :height => 30, :width => 150 do |e|
 				@counter2.text = e.text()
+				ending = @counter2.text.to_s
 			end
 		end
-		@counter = strong( "0" )
-		@counter2 = strong( "" )
-		para @counter, " what? ", @counter2, "."
+		
+	end
 
+		@counter = strong( "0" )
+		@counter2 = strong( "0" )
+		para "Closing ticket range ", @counter, " to ", @counter2, "."
+		stack :margin => 10, do 
 		button "Submit" do
-			alert '@counter is the size, and @counter2 is the actual contents. *\o/*'
+			content = system('powershell C:\scripts\Close-Range.ps1 #{start} #{ending}')
+			
 		end
 	end
 end
